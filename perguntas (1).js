77d545e9dd2d818ed356c68f9ec5b3bdcec9a4d3
Yarasantos24 <yaran8659@gmail.com>
2024-10-21 09:46:59 -0300

    Add files via upload

---
+++ perguntas (1).js
@@ -0,0 +1,48 @@
+criaCartao(
+   'psicologia',
+   'O que e a psicologia?',
+   ' Psicologia e a ciencia que estuda o comportamento humano e seus processos mentais. Melhor dizendo, a Psicologia estuda o que motiva o comportam'.
+)
+
+criaCartao(
+    'psicologia',
+    'Quais sao os principais metodos De pesquisa ultilizaos em psicologia?',
+    ' Incluem experimentacao, observacao, entrevistas, questionarios e estudos e caso podendo ser  qualitativos ou quantitativos'
+
+)
+  
+    
+  criaCartao (
+      'psicologia',
+  'Como psicologia do desenvolvimento explica as mudancas ao longo da vida?',
+  ' Exmina as mudancas ao longo da vida em aspectos fisicos, cognitivos, emocionais e sociais, investigando diferentes estagios e deenvolvimento',
+  
+   )
+  
+   criaCartao (
+   'psicologia',
+   'Qual a diferenca entre psicologia clinica e psiquiatria',
+   ' a psicologia clinica foca no tratamento de transtornos mentais com terapia, enquanto a psiquiatria e uma especialidade medica que pode presevar medicamentos',
+   )
+   criacartao (
+    'psicologia',
+    'o que sao transtornos psicologico e como eles sao diagnosticado',
+    ' condicoes que afetam pensamentos e comportamentos, diagnosticadas com base em criterio do DSM-5, considerando sintomas e impactos funcinal',
+   )
+
+   criacartao (
+    'psicologia', 
+    'como a psicologia pode ser aplicada na educacao para melhorar o aprendizado',
+    ' incluem a hierarquia de necessidade de Maslow, a teoria da autodeterminacao e a teoria da expectividade',
+   )
+
+   cricartao (
+    'psicologia',
+    ' quais sao as teorias principais da motivacao humana',
+    ' incluem a hierarquia de necessidade de Maslow, a teoria da autodeterminacao e a teoria da exoectativa',
+   )
+
+   criacartao (
+    'psicologia',
+    'como a psicologia social ivestiga a influencia do grupo sobre o comportamento individual',
+   )
