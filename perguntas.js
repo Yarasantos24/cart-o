fd9bd29815951dc24f77ce522e82c30a230c24ef
Yarasantos24 <yaran8659@gmail.com>
2024-10-21 09:13:51 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,29 +1,34 @@
-criaCartao
-    psicológia,(
-        'O que é a psicológia?'
+criaCartao(
+   'psicológia',
+   'O que é a psicológia?',
+   'Psicologia é a ciência que estuda o comportamento humano e seus processos mentais. Melhor dizendo, a Psicologia estuda o que motiva o comportam'.
+)
 
-   A psicologia é a ciência que estuda o comportamento humano e seus processos mentais. Melhor dizendo, a Psicologia estuda o que motiva o comportamento humano"
-  
-    )
+criaCartao(
+    'psicológia',
+    'Quais sao os principais métodos De pesquisa ultilizaos em psicológia?',
+    ' Incluem experimentação, observação, entrevistas, questionários e estudos e caso podendo ser  qualitativos ou quantitativos'
+
+)
   
   
   criaCartao (
     'psicológia',
-  Quais sao os principais métodos De pesquisa ultilizaos em psicológia?
-   Incluem experimentação, observação, entrevistas, questionários e estudos e caso podendo ser  qualitativos ou quantitativos.'
+  'Quais sao os principais métodos De pesquisa ultilizaos em psicológia?',
+  'Incluem experimentação, observação, entrevistas, questionários e estudos e caso podendo ser  qualitativos ou quantitativos',
   
-  
+
   )
     
   criaCartao (
-      psicológia
-  Como psicológia do desenvolvimento explica as mudanças ao longo da vida?'
-  Exmina as mudanças ao longo da vida em aspectos fisicos, cognitivos, emocionais e sociais, investigando diferentes estágios e deenvolvimento.
+      'psicológia',
+  'Como psicológia do desenvolvimento explica as mudanças ao longo da vida?',
+  'Exmina as mudanças ao longo da vida em aspectos fisicos, cognitivos, emocionais e sociais, investigando diferentes estágios e deenvolvimento',
   
    )
   
    criaCartao 
-   psicológia
-   Qual a diferença entre psicológia clínica e psiquiatria?
-    A psicológia clínica foca no tratamento de transtorno mentais com terapia, enquanto a psiquiatria é uma especilidade médica que pode prescrever meicamentos
+   'psicológia',
+   'Qual a diferença entre psicológia clínica e psiquiatria?',
+   ' A psicológia clínica foca no tratamento de transtorno mentais com terapia, enquanto a psiquiatria é uma especilidade médica que pode prescrever meicamentos',
   
