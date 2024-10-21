5d581ef1646e8e8c38c44d3ec1461c8f9020bc1b
Yarasantos24 <yaran8659@gmail.com>
2024-10-21 08:51:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,29 @@
+criaCartao
+    psicológia,(
+        'O que é a psicológia?'
+
+   A psicologia é a ciência que estuda o comportamento humano e seus processos mentais. Melhor dizendo, a Psicologia estuda o que motiva o comportamento humano"
+  
+    )
+  
+  
+  criaCartao (
+    'psicológia',
+  Quais sao os principais métodos De pesquisa ultilizaos em psicológia?
+   Incluem experimentação, observação, entrevistas, questionários e estudos e caso podendo ser  qualitativos ou quantitativos.'
+  
+  
+  )
+    
+  criaCartao (
+      psicológia
+  Como psicológia do desenvolvimento explica as mudanças ao longo da vida?'
+  Exmina as mudanças ao longo da vida em aspectos fisicos, cognitivos, emocionais e sociais, investigando diferentes estágios e deenvolvimento.
+  
+   )
+  
+   criaCartao 
+   psicológia
+   Qual a diferença entre psicológia clínica e psiquiatria?
+    A psicológia clínica foca no tratamento de transtorno mentais com terapia, enquanto a psiquiatria é uma especilidade médica que pode prescrever meicamentos
+  
